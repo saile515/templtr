8b5964b6cf7bf5e084e33a760584c539639319b0
saile515 <elias.jorgensen2006@gmail.com>
2023-03-16 12:30:46 +0100

    Close nav when tapping outside of it

--- docs/public/js/nav.js
+++ docs/public/js/nav.js
@@ -1,12 +1,10 @@
-const navButton = document.getElementById("open-nav");
-
 let isOpen = false;
 
-navButton.onclick = () => {
+function toggleNav() {
 	isOpen = !isOpen;
 	if (isOpen) {
 		document.querySelector("#navbar nav").classList.add("open");
 	} else {
 		document.querySelector("#navbar nav").classList.remove("open");
 	}
-};
+}
